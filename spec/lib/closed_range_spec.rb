e0a1c54ba75bb643174dc16b5658de297e0175b8
ShinnosukeKomiya <komiya.shinnosuke@lmi.ne.jp>
2022-12-20 15:44:05 +0900

    chore: 初期値のセットの段階で数の大小関係を考慮出来ることを前提にしたテストケースを作成

--- spec/lib/closed_range_spec.rb
+++ spec/lib/closed_range_spec.rb
@@ -1,13 +1,32 @@
 describe ClosedRange do
-  let(:lower_num) { 3 }
-  let(:upper_num) { 7 }
+  let(:initial_num) { 1 }
+  let(:second_num) { 2 }
+  let(:instance) { ClosedRange.new(initial_num, second_num) }
   describe '#initialize' do
-    subject { ClosedRange.new(lower_num, upper_num) }
-    example '下端点が正しくセットされること' do
-      expect(subject.instance_variable_get(:@lower_num)).to eq lower_num
+    subject { instance }
+    context '数の大小関係が 第1引数 < 第2引数 の場合' do
+      example '下端点と上端点が正しくセットされること' do
+        # MEMO: これは一緒に確認したい項目なので、一緒にexpectで検査する
+        expect(subject.instance_variable_get(:@lower_num)).to eq initial_num
+        expect(subject.instance_variable_get(:@upper_num)).to eq second_num
+      end
     end
-    example '上端点が正しくセットされること' do
-      expect(subject.instance_variable_get(:@upper_num)).to eq upper_num
+    context '数の大小関係が 第1引数 > 第2引数 の場合' do
+      let(:initial_num) { 2 }
+      let(:second_num) { 1 }
+      example '下端点と上端点が正しくセットされること' do
+        expect(subject.instance_variable_get(:@lower_num)).to eq second_num
+        expect(subject.instance_variable_get(:@upper_num)).to eq initial_num
+      end
+    end
+    context '数の大小関係が 第1引数 = 第2引数 の場合' do
+      let(:initial_num) { 3 }
+      let(:second_num) { 3 }
+      example '下端点と上端点が正しくセットされること' do
+        expect(subject.instance_variable_get(:@lower_num)).to eq second_num
+        expect(subject.instance_variable_get(:@upper_num)).to eq initial_num
+        expect(subject.instance_variable_get(:@upper_num)).to eq subject.instance_variable_get(:@lower_num)
+      end
     end
   end
 end
