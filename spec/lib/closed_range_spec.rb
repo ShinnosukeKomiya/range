74fb87a9cd8bc06c2f1d99b9dba2378f7d290ddf
ShinnosukeKomiya <komiya.shinnosuke@lmi.ne.jp>
2022-12-20 16:08:49 +0900

    refactor: subjectではなく、instanceを生成しているのが分かりやすいようにした

--- spec/lib/closed_range_spec.rb
+++ spec/lib/closed_range_spec.rb
@@ -1,31 +1,30 @@
 describe ClosedRange do
+  let(:closed_range_instance) { ClosedRange.new(initial_num, second_num) }
   describe '#initialize' do
-    let(:closed_range_instance) { ClosedRange.new(initial_num, second_num) }
-    subject { closed_range_instance }
     context '数の大小関係が 第1引数 < 第2引数 の場合' do
       let(:initial_num) { 1 }
       let(:second_num) { 2 }
       example '下端点と上端点が正しくセットされること' do
         # MEMO: これは一緒に確認したい項目なので、一緒にexpectで検査する
-        expect(subject.instance_variable_get(:@lower_num)).to eq initial_num
-        expect(subject.instance_variable_get(:@upper_num)).to eq second_num
+        expect(closed_range_instance.instance_variable_get(:@lower_num)).to eq initial_num
+        expect(closed_range_instance.instance_variable_get(:@upper_num)).to eq second_num
       end
     end
     context '数の大小関係が 第1引数 > 第2引数 の場合' do
       let(:initial_num) { 2 }
       let(:second_num) { 1 }
       example '下端点と上端点が正しくセットされること' do
-        expect(subject.instance_variable_get(:@lower_num)).to eq second_num
-        expect(subject.instance_variable_get(:@upper_num)).to eq initial_num
+        expect(closed_range_instance.instance_variable_get(:@lower_num)).to eq second_num
+        expect(closed_range_instance.instance_variable_get(:@upper_num)).to eq initial_num
       end
     end
     context '数の大小関係が 第1引数 = 第2引数 の場合' do
       let(:initial_num) { 3 }
       let(:second_num) { 3 }
       example '下端点と上端点が正しくセットされること' do
-        expect(subject.instance_variable_get(:@lower_num)).to eq second_num
-        expect(subject.instance_variable_get(:@upper_num)).to eq initial_num
-        expect(subject.instance_variable_get(:@upper_num)).to eq subject.instance_variable_get(:@lower_num)
+        expect(closed_range_instance.instance_variable_get(:@lower_num)).to eq second_num
+        expect(closed_range_instance.instance_variable_get(:@upper_num)).to eq initial_num
+        expect(closed_range_instance.instance_variable_get(:@upper_num)).to eq closed_range_instance.instance_variable_get(:@lower_num)
       end
     end
   end
