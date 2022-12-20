33764d8a63cecca2852885e7a54bd46a586b04ff
ShinnosukeKomiya <komiya.shinnosuke@lmi.ne.jp>
2022-12-20 15:44:39 +0900

    feat: 引数の大小関係を考慮して、インスタンス変数をセットできるようにした

--- lib/closed_range.rb
+++ lib/closed_range.rb
@@ -1,6 +1,11 @@
 class ClosedRange
   def initialize(initial_num, second_num)
-    @lower_num = initial_num
-    @upper_num = second_num
+    if initial_num > second_num
+      @lower_num = second_num
+      @upper_num = initial_num
+    else
+      @lower_num = initial_num
+      @upper_num = second_num
+    end
   end
 end
